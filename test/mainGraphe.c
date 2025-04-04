16e1c1d3806a054564019f5ba878bbf3266d819a
jugomis <julien.gomis031015@gmail.com>
2025-04-04 13:48:24 +0200

    giant terminé

--- test/mainGraphe.c
+++ test/mainGraphe.c
@@ -28,30 +28,32 @@ int main(int argc, char* argv[]){
 
     */
     
-    graphe g_aleat = aleatoire(9, 1.7);//, G2 = lireGraphe("../data/maison.txt");
-    //creerDotGraphe(G2, "maison");
-    //dessinerGraphe(G2, "maison");
+    graphe g_aleat = aleatoire(9, 1.7), G2 = lireGraphe("../data/maison.txt");
+    creerDotGraphe(G2, "maison");
+    dessinerGraphe(G2, "maison");
 
     int * maximum = malloc(sizeof(int));
     int * num_giant = malloc(sizeof(int));
-    int * sommets_giant = giant(g_aleat, maximum, num_giant);
+    int * sommets_giant = giant(G2, maximum, num_giant);
 
     //(G2, maximum, sommets_comp);
     //printf("Le graphe maison a %d composantes connexes\nSa composante géante est de %d sommets\n", nb_comp, *maximum);
-/*  printf("[");
+    printf("Sommets graphe Maison :[");
     for (int it=0; it<G2.nbs-1; it++)
-        printf("%d, ", sommets_comp[it]);
-    printf("%d]", sommets_comp[G2.nbs-1]);
-*/
-    
+        printf("%d, ", sommets_giant[it]);
+    printf("%d]\n", sommets_giant[G2.nbs-1]);
+
+    printf("La composante géante est d'ordre %d\nC'est la composante numéro %d \n", *maximum, *num_giant);
+
+    sommets_giant = giant(g_aleat, maximum, num_giant);
     dessinerGraphe(g_aleat, "graphe_aleatoire");
     //printf("Le graphe aleatoire a %d composantes connexes\nSa composante géante est de %d sommets\n", nb_comp, *maximum);
-    printf("Sommets graphe : [");
+    printf("Sommets graphe_aleatoire : [");
     for (int it=0; it<g_aleat.nbs-1; it++)
         printf("%d, ", sommets_giant[it]);
     printf("%d]\n", sommets_giant[g_aleat.nbs-1]);
 
-    printf("La composasnte géante est d'ordre %d\nC'est la composante numéro %d \n", *maximum, *num_giant);
+    printf("La composante géante est d'ordre %d\nC'est la composante numéro %d \n", *maximum, *num_giant);
 
     //freeGraphe(G2);
     freeGraphe(g_aleat);
